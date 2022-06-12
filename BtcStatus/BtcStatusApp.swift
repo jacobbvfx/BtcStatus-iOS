7a9dd5121415d7fd6c62fd64188438a403d81ec7
Jacob <38792138+shadoweG@users.noreply.github.com>
2022-06-12 14:54:41 +0200

    Initial Commit

---
+++ BtcStatus/BtcStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  BtcStatusApp.swift
+//  BtcStatus
+//
+//  Created by jacob on 12/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BtcStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
